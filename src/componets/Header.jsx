c75b7f17991c57d26e329bb5b1d295fe637c3907
Carina Cunha <carina.cunha13@gmail.com>
2022-09-20 23:57:53 -0300

    Req 3 in progress

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -2,7 +2,20 @@ import React, { useContext } from 'react';
 import GlobalContext from '../context/GlobalContext';
 
 function Header() {
-  const { setInputName, planets, setPlanetsList } = useContext(GlobalContext);
+  const {
+    planets,
+    planetsList,
+    setInputName,
+    setPlanetsList,
+    columns,
+    valueColumn,
+    setValueColumn,
+    valueComparation,
+    setValueComparation,
+    comparations,
+    valueNumber,
+    setValueNumber,
+  } = useContext(GlobalContext);
 
   const handleChange = ({ target }) => {
     const { value } = target;
@@ -11,8 +24,44 @@ function Header() {
     const filterByName = planets.filter(
       (planet) => planet.name.toLowerCase().includes(name),
     );
-
     setPlanetsList(filterByName);
+  };
+
+  const handleColumns = ({ target }) => {
+    const { value } = target;
+    setValueColumn(value);
+  };
+
+  const handleComparations = ({ target }) => {
+    const { value } = target;
+    setValueComparation(value);
+  };
+
+  const handleNumber = ({ target }) => {
+    const { value } = target;
+    setValueNumber(value);
+  };
+
+  const handleClickFilter = () => {
+    let planetsFiltered = [];
+    if (valueComparation === 'maior que') {
+      planetsFiltered = planetsList
+        .filter((planet) => planet[valueColumn] !== 'unknown'
+      && Number(planet[valueColumn]) > Number(valueNumber));
+    }
+
+    if (valueComparation === 'menor que') {
+      planetsFiltered = planetsList
+        .filter((planet) => planet[valueColumn] !== 'unknown'
+      && Number(planet[valueColumn]) < Number(valueNumber));
+    }
+
+    if (valueComparation === 'menor que') {
+      planetsFiltered = planetsList
+        .filter((planet) => planet[valueColumn] !== 'unknown'
+      && Number(planet[valueColumn]) === Number(valueNumber));
+    }
+    setPlanetsList(planetsFiltered);
   };
 
   return (
@@ -25,8 +74,63 @@ function Header() {
             placeholder="Digite o nome do planeta"
             onChange={ handleChange }
           />
+          <legend>
+            Colunas
+            <select
+              data-testid="column-filter"
+              value={ valueColumn }
+              onChange={ handleColumns }
+            >
+              {
+                columns.map((col) => (
+                  <option
+                    value={ col }
+                    key={ col }
+                  >
+                    {col}
+                  </option>
+                ))
+              }
+            </select>
+          </legend>
+          <legend>
+            Intervalo
+            <select
+              data-testid="comparison-filter"
+              value={ valueComparation }
+              onChange={ handleComparations }
+            >
+              {
+                comparations.map((comp, index) => (
+                  <option
+                    value={ comp }
+                    key={ index }
+                  >
+                    {comp}
+                  </option>
+                ))
+              }
+            </select>
+          </legend>
+          <legend>
+            Valor
+            <input
+              data-testid="value-filter"
+              type="number"
+              min="0"
+              value={ valueNumber }
+              onChange={ handleNumber }
+            />
+          </legend>
         </section>
       </form>
+      <button
+        data-testid="button-filter"
+        type="button"
+        onClick={ handleClickFilter }
+      >
+        Filtrar
+      </button>
     </header>
   );
 }
--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -5,8 +5,21 @@ import useFetch from '../hooks/useFetch';
 
 function Provider({ children }) {
   const [planets] = useFetch();
-  const [inputName, setInputName] = useState('');
   const [planetsList, setPlanetsList] = useState([]);
+  const [inputName, setInputName] = useState('');
+  const [columns, setColumns] = useState(
+    [
+      'population',
+      'orbital_period',
+      'diameter',
+      'rotation_period',
+      'surface_water',
+    ],
+  );
+  const [valueColumn, setValueColumn] = useState('orbital_period');
+  const [valueComparation, setValueComparation] = useState('menor que');
+  const comparations = ['maior que', 'menor que', 'igual a'];
+  const [valueNumber, setValueNumber] = useState('0');
 
   useEffect(() => {
     setPlanetsList(planets);
@@ -18,6 +31,16 @@ function Provider({ children }) {
     setInputName,
     inputName,
     setPlanetsList,
+    columns,
+    setColumns,
+    valueColumn,
+    setValueColumn,
+    valueComparation,
+    setValueComparation,
+    comparations,
+    valueNumber,
+    setValueNumber,
+
   };
 
   return (
